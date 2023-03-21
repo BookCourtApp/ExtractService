e00e23227e85e28f3b4429dcdf1e1d759c42d8e5
ZadVes <105089730+ZadVes@users.noreply.github.com>
2023-03-21 22:39:18 +0300

    Добавил изменения в функции: ExtractorWorker, IExtractorWorker

--- Core/ExtractorWorker.cs
+++ Core/ExtractorWorker.cs
@@ -1,19 +1,30 @@
 ﻿namespace Core;
 
-/// <summary>
-/// интерфейс ExtractorWorker
-/// </summary>
-public interface ExtractorWorker
+public class ExtractorWorker : IExtractorWorker
 {
-    /// <summary>
-    /// Запуск Extractor по заданным условиям
-    /// </summary>
-    /// <returns>Возвращает результат работы с условиями</returns>
-    public String ExtractData();
-    
-    
-    
-    
-    
-    
+    private IExtractorFactory _factory;
+
+    public void ExtractData(string setings)
+    {
+        
+        IExtractor extractor = _factory.CreateExtractor(setings);
+        bool examExtract = extractor.IsEndData(); //todo: Записываем данные о массиве в переменную examExtract 
+        while (examExtract == true)
+        {
+            Guid extractorBatch = extractor.ExtractNextBatch();
+            //todo: Данный while должен принимать значение Batch и записывать его в репозиторий (ExtractorResultRepository)
+            // Если данные в Batch отсутствуют, меняем значение переменной examExtract == false
+            if (extractorBatch == null)
+            {
+                break;
+            }
+        }
+        
+    }
+
+    public ExtractorWorker(IExtractorFactory factory)
+    {
+        _factory = factory;
+    }
+
 }
