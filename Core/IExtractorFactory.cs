836b028f3dd6504f7ac4ff76eaf9343438783c03
Kosilov Aleksandr <Kosilov.AA@dns-shop.ru>
2023-03-21 20:03:30 +1000

    Добавил интерфейс фабрики Экстрактора

---
+++ Core/IExtractorFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace Core;
+
+/// <summary>
+/// интерфейс фабрики для экстрактора
+/// </summary>
+public interface IExtractorFactory
+{
+    /// <summary>
+    /// Создание экстрактора по настройкам
+    /// </summary>
+    /// <param name="extractorSettings">Настройки для создания экстрактора</param>
+    /// <returns>Экстрактор с определенным типом</returns>
+    public IExtractor CreateExtractor(string extractorSettings); // todo: заменить string на норм типы
+}
