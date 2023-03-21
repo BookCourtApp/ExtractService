9420f3c8f2aad67827c87d9e9186d42ef94ef263
ZadVes <105089730+ZadVes@users.noreply.github.com>
2023-03-21 20:06:44 +0300

    Добавлен интерфейс ExtractorWorker

---
+++ Core/ExtractorWorker.cs
@@ -0,0 +1,19 @@
+﻿namespace Core;
+
+/// <summary>
+/// интерфейс ExtractorWorker
+/// </summary>
+public interface ExtractorWorker
+{
+    /// <summary>
+    /// Запуск Extractor по заданным условиям
+    /// </summary>
+    /// <returns>Возвращает результат работы с условиями</returns>
+    public String ExtractData();
+    
+    
+    
+    
+    
+    
+}
