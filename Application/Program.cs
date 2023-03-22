71726b0353aadcd504265f74aa80ca17f8b21355
Kosilov Aleksandr <Kosilov.AA@dns-shop.ru>
2023-03-22 21:18:35 +1000

    Добавил Application с DI, Добавил ДБ контекст и настроил на работу с sqlite

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,3 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using InfrastructureProject;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+// создание приложения с DI контейнером 
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>   // настройка сервисов
+    {
+        // Добавил dbContext для работы с БД
+        services.AddDbContext<ApplicationContext>(options =>
+        {
+            // захардкодил путь до БД, перед запуском нужно поставить свой путь.В существуюзей папке будет создаваться/использоваться .db файл как БД sqlite
+            // sqlite это in-memory db
+            options.UseSqlite(@"Data Source=C:\Users\ted70\Work\BookCourt\db\myDb.db");      
+        });
+    }).Build();
+
+#region DbDepednencyExample
+
+// получаем из зависимостей контекст
+// var context = host.Services.GetService<ApplicationContext>();
+// 
+// context.Books.Add(new Book()
+// {
+//     Name = "",
+//     Author = "aaa",
+//     Description = "",
+//     Genre = "",
+//     Image = "",
+//     ISBN = "",
+//     SourceName = ""
+// });
+// context.SaveChanges();
+//
+// Console.WriteLine(context.Books.Count());
+
+
+#endregion
+
+await host.RunAsync();
