38f4de798b794d9ea3cd4c246d6015049c4a5b50
Alex Fomin <alexkillzone53@gmail.com>
2023-03-23 19:44:06 +1000

    Исправил файл AbstractExtractor

--- ExtractorProject/Extractors/AbstractExtractor.cs
+++ ExtractorProject/Extractors/AbstractExtractor.cs
@@ -1,8 +1,6 @@
 using ExtractorService.Models;
 
 namespace ExtractorService.Extractor{
-    public abstract class AbstractExtractor : ExtractorReviews, ExtractorBooks, IExtractor{
-        public AbstractExtractor(ExtractorSettings); 
     public abstract class AbstractExtractor{
         /// <summary>
         /// Конструктор для инициализации парсера
