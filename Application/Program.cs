77b1d8e137a7058c0c38f86bf0c85e5859484c4b
Kosilov Aleksandr <Kosilov.AA@dns-shop.ru>
2023-03-23 22:46:44 +1000

    add config to app

--- Application/Program.cs
+++ Application/Program.cs
@@ -3,19 +3,22 @@
 using ExtractorService.Models;
 using InfrastructureProject;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 // создание приложения с DI контейнером 
 using IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>   // настройка сервисов
+    .ConfigureServices((context, services)  =>   // настройка сервисов
     {
         // Добавил dbContext для работы с БД
         services.AddDbContext<ApplicationContext>(options =>
         {
+            var config = context.Configuration;
             // захардкодил путь до БД, перед запуском нужно поставить свой путь.В существуюзей папке будет создаваться/использоваться .db файл как БД sqlite
             // sqlite это in-memory db
-            options.UseSqlite(@"Data Source=C:\Users\ted70\Work\BookCourt\db\myDb.db");      
+            var connectionString = config.GetValue<string>("SqliteConnectionString");
+            options.UseSqlite(connectionString);      
         });
     }).Build();
 
