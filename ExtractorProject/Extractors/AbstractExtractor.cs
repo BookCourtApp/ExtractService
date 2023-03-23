a1d83ee9bc5df41196702f86109fad58e01a631f
Enorezero <98647618+Enorezero@users.noreply.github.com>
2023-03-23 19:36:54 +1000

    Отредактировал Файлы

--- ExtractorProject/Extractors/AbstractExtractor.cs
+++ ExtractorProject/Extractors/AbstractExtractor.cs
@@ -2,7 +2,7 @@ using ExtractorService.Models;
 
 namespace ExtractorService.Extractor{
     public abstract class AbstractExtractor : ExtractorReviews, ExtractorBooks, IExtractor{
-        public AbstractExtractor(ExtractorSettings) 
+        public AbstractExtractor(ExtractorSettings); 
     }
 }
 
