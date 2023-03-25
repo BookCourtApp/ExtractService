dc374328c545b1dbc2edb3941cc8603922e3ea52
ZadVes <105089730+ZadVes@users.noreply.github.com>
2023-03-25 21:31:47 +0300

    Добавил ветку ExtrctorBook24

---
+++ Parser/Book.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace BookParser;
+
+public class Book
+{
+    public string Name { get; set; }
+
+    public string Author { get; set; }
+
+    public string Description { get; set; }
+
+    public int Price { get; set; }
+
+    public int Remainder { get; set; }
+
+    public string SourceName { get; set; }
+    
+    public string Image { get; set; }
+
+    public string NumberOfPages { get; set; }
+
+    public string Genre { get; set; }
+
+    public int NumberOfImages { get; set; }
+
+    public string CoverType { get; set; }
+
+    public string Language { get; set; }
+
+    public string ISBN { get; set; }
+
+    public string Publisher { get; set; }
+
+    public string PublisherYear { get; set; }
+
+    public string Series { get; set; }
+
+    public string AgeRestrictions { get; set; }
+
+    public string Format { get; set; }
+
+    public string Weight { get; set; }
+
+    public int Sales { get; set; }
+
+    public string Reviews { get; set; }
+
+    public string Rating { get; set; }
+
+    public string VendorCode { get; set; }
+
+    public DateTime ParsingDate { get; set; }
+
+}
---
+++ Parser/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Parser;
+
+Console.WriteLine("Hello, World!");
+
+ExtractorBook24 parser = new ExtractorBook24();
+
+parser.StartParsingAsync();
