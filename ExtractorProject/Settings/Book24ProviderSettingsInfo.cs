671cde8520a98fc270eb88572cddff2c2963e21d
Alex Fomin <alexkillzone53@gmail.com>
2023-04-09 23:20:29 +1000

    Добавил экстрактор для book24

---
+++ ExtractorProject/Settings/Book24ProviderSettingsInfo.cs
@@ -0,0 +1,22 @@
+﻿using Core.Settings;
+
+namespace ExtractorProject.Settings;
+
+/// <summary>
+/// Настройки для провайдера ресурсов для Book24
+/// </summary>
+public class Book24ProviderSettingsInfo : IProviderSettingsInfo
+{
+    /// <summary>
+    /// Страница с пейджами без номера
+    /// </summary>
+    public string Catalog {get;set;}
+    /// <summary>
+    /// Стартовая страница для парсинга
+    /// </summary>
+    public int MinPage { get; set; }
+    /// <summary>
+    /// Конечная страница для парсинга
+    /// </summary>
+    public int MaxPage { get; set; }
+}
